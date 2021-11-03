196fca5c5be3daeeb63b5319d63fbffe6af1bc66
Gabriel <omnifarter@gmail.com>
2021-11-03 18:08:46 +0800

    update route

--- rails_project/config/routes.rb
+++ rails_project/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'recording/save_video'
   get 'recording/index'
   root 'home#index'
-  mount ActionCable.server => '/recording/index'
+  # mount ActionCable.server => '/recording/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
