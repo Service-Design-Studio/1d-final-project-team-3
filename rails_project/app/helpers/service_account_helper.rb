1458a36a34c5765c5b8257a9c1f58de9e19ee686
dragernix <pang.bangyong@gmail.com>
2021-11-15 21:10:16 +0800

    keyfile

--- rails_project/app/helpers/service_account_helper.rb
+++ rails_project/app/helpers/service_account_helper.rb
@@ -6,7 +6,7 @@ module ServiceAccountHelper
                 p 'initializing service account'
                 scope = 'https://www.googleapis.com/auth/cloud-platform'
                 @authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-                json_key_io: File.open(Rails.root.join("keys","d-final-project-team-3-3f8b143456d4.json").to_s),
+                json_key_io: File.open(Rails.root.join("keys","d-final-project-team-3-3f8b143456d4.keyfile").to_s),
                 scope: scope)
                 @token = @authorizer.fetch_access_token!
                 p 'initialized service account'
