087d334dc2afdfcf4169b6c2f220673010f7c6c1
Gabriel <omnifarter@gmail.com>
2021-11-24 18:09:21 +0800

    change sqlite3 to pg

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -12,14 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2021_11_08_125830) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -38,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_125830) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -54,7 +51,7 @@ ActiveRecord::Schema.define(version: 2021_11_08_125830) do
     t.integer "phone_IMEI"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_recordings_on_user_id"
   end
 
