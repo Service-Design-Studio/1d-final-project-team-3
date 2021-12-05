667dda691c006d711dc1d6572d7808dac1ef4c2c
Gabriel <1003466@sutd.edu.sg>
2021-12-05 07:06:36 +0000

    add new transcription box design inside recording edit

--- rails_project/app/javascript/Recording/logs.js
+++
@@ -1,9 +0,0 @@
-$('.clickable-row').click(function() {
-    window.document.location = $(this).data("link")
-  });
-
-  $('tr').click(function() {
-    alert("table row clicked")
-    //window.document.location = $(this).data("link")
-    windows.location.href = edit_recording_path(record)
-  });
